9570d33b368814b3c9cc3cfe20e669c0727d2299
Krzysztof Jurkiewicz <huntekah@gmail.com>
2016-11-24 09:31:42 +0100

    Now I can extract letters(as a group) from the rest of the image

--- step_two.py
+++ step_two.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import random
 
-SHOW_CONTOURS = True
+SHOW_CONTOURS = False
 
 class plamka:
     def __init__(self,image):
@@ -21,17 +21,18 @@ class plamka:
                                                   # (see img 9 with this line commented),
                                                   # you choose the biggest white contour
         #self.print_values_on_contour(self.contour)
-
+        ''' wziac convex i po prostu wymnożyć, usunac kolory.'''
         '''might be usefull to look for numbers in other colored areas'''
         # just to learn kwargs! :D
-        self.image, self.altered_image = self.trim_to_mask(source=self.image, mask=self.altered_image)
+        self.show()
         self.image = self.image_X_mask()
+        self.show()
+        self.image, self.altered_image = self.trim_to_mask(source=self.image, mask=self.convex(self.altered_image))
         #self.gray_image = rgb2gray(self.image)
+        self.altered_image = self.erase_colors(0.0)
+        #self.open(5)
+        self.find_contours()
 
-        #self.altered_image = self.erase_colors(0.0)
-        #self.image = self.new_image(self.contour,self.image)
-        #self.altered_image = self.new_image(self.contour, self.altered_image)
-        #self.find_contours()
         #self.convex() # do I need you?
 
 
@@ -78,7 +79,7 @@ class plamka:
         else:
             return source[bbox[0]: bbox[2], bbox[1]: bbox[3]]
 
-    def open(self):
+    def open(self, alpha=2.5):
         try:
             #self.altered_image = np.array(self.image[:,:,1])
             seed = (min(len(self.altered_image),len(self.altered_image[0]))**2)/10000
@@ -87,10 +88,12 @@ class plamka:
 
 
 
-            self.altered_image = morphology.closing(self.altered_image, morphology.disk(seed)/3)
-            self.altered_image = morphology.opening(self.altered_image, morphology.disk(seed) / 3)
+            self.altered_image = morphology.closing(self.altered_image, morphology.disk(seed)/ alpha)
+            self.altered_image = morphology.opening(self.altered_image, morphology.disk(seed) / alpha)
             #self.altered_image = filters.gaussian(self.altered_image,sigma = seed)
         except AssertionError as e:
+            self.altered_image = morphology.closing(self.altered_image, morphology.disk(10))
+            self.altered_image = morphology.opening(self.altered_image, morphology.disk(10))
             print("seed size too big: {}! \nresize image please!".format(seed))
             print(e)
 
@@ -115,10 +118,10 @@ class plamka:
 
 
     def convex(self, image=None):
-        if(image):
-            self.altered_image = morphology.convex_hull_image(image)
+        if image is None:
+            return morphology.convex_hull_image(image)
         else:
-            self.altered_image = morphology.convex_hull_image(self.altered_image)
+            return morphology.convex_hull_image(self.altered_image)
 
     def get_biggest_contour(self):
         max_area = 0
@@ -129,30 +132,44 @@ class plamka:
             if max_area < area:
                 max_area = area
                 result = contour
-        assert (result != None)
+        assert not (result is None)
 
         return result
 
     def image_X_mask(self, **kwargs):
         options = {
             'source'    : self.image,
-            'mask'      : self.altered_image
+            'mask'      : self.altered_image,
+            'background': 1
         }
         options.update(kwargs)
 
         image = options['source']
         altered_image = options['mask']
+        background = options['background']
 
         for i, row in enumerate(image):
             for j, pixel in enumerate(row):
-                image[i][j] = [pixel[0]*altered_image[i][j],\
-                               pixel[1]*altered_image[i][j],\
-                               pixel[2]*altered_image[i][j]]
+                if altered_image[i][j] == 0:
+                    image[i][j] = [255 * background,
+                                   255 * background,
+                                   255 * background]
+                else:
+                    image[i][j] = [pixel[0] * altered_image[i][j],\
+                               pixel[1] * altered_image[i][j],\
+                               pixel[2] * altered_image[i][j]]
         return image
 
-    def erase_colors(self,threshold=0.4):
-        img = np.array(self.gray_image)
-        for i, row in enumerate(self.image):
+    def erase_colors(self,threshold=0.4, **kwargs):
+        options = {
+            'image': self.image,
+        }
+        options.update(kwargs)
+
+        image = options['image']
+
+        img = np.ones((len(image), len(image[0])))
+        for i, row in enumerate(image):
             for j, rgb in enumerate(row):
                 r = rgb[0]
                 g = rgb[1]
@@ -172,8 +189,8 @@ class plamka:
         if SHOW_CONTOURS:
             for contour in self.contours:
                 self.plots[0].plot(contour[:, 1], contour[:, 0], linewidth=2, zorder=1)
-            #    self.plots[1].plot(contour[:, 1], contour[:, 0], linewidth=2, zorder=1)
-            self.plots[1].plot(self.contour[:, 1], self.contour[:, 0], linewidth=2, zorder=1)
+                self.plots[1].plot(contour[:, 1], contour[:, 0], linewidth=2, zorder=1)
+          #  self.plots[1].plot(self.contour[:, 1], self.contour[:, 0], linewidth=2, zorder=1)
 
         self.plots[0].tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off',
                                             labelleft='off', labelbottom='off')
@@ -194,7 +211,7 @@ if __name__ == "__main__":
 
     #for i in range(1,1):
     i=random.randint(1,14) # choose one of 14 images randomly
-    i = 2 #to chooose specyfic image
+    i = 5 #to chooose specyfic image
     image = get_image(images+str(i)+".jpg", False)
     #image = get_image("pictures_small/img (7).jpg", False)
     Plamka = plamka(image)
